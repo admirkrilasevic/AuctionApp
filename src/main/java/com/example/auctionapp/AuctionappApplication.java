3f0c69a46ed382b1ba17eb7cad512cf310fa6882
admirkrilasevic <admirkrilasevic@gmail.com>
2021-10-19 18:18:12 +0200

    initial commit

---
+++ src/main/java/com/example/auctionapp/AuctionappApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionappApplication.class, args);
+	}
+
+}
