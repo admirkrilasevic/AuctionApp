9f5cb75fa7f263f9de5881bdca48dd18b537e0f8
admirkrilasevic <admirkrilasevic@gmail.com>
2021-10-28 14:26:52 +0200

    implemented routing for navbar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,16 +1,34 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header';
 import HeaderNavbar from './components/Navbar';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Account from './pages/Account';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HeaderNavbar />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <HeaderNavbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/account">
+            <Account />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/main/frontend/src/components/Navbar.js
+++ src/main/frontend/src/components/Navbar.js
@@ -2,21 +2,22 @@ import styles from './Navbar.module.css'
 import {Container, Col,  Row } from 'react-bootstrap';
 import SearchBar from "material-ui-search-bar";
 import logo from '../assets/auction-app-logo.png';
+import {Link} from 'react-router-dom';
 
 const HeaderNavbar = () => {
     return(
         <Container className={styles.navbarContainer}>
             <Row className={styles.navbar}>
                 <Col>
-                    <img className={styles.logo} src={logo} alt="Logo"></img>
+                    <Link to="/"><img className={styles.logo} src={logo} alt="Logo"></img></Link> 
                 </Col>
                 <Col xs={6}>
                     <SearchBar className={styles.searchbar} placeholder="Try enter: Shoes" />
                 </Col>
                 <Col className={styles.buttonsContainer}>
-                    <a href="">Home</a> 
-                    <a href="">Shop</a> 
-                    <a href="">My account</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/shop">Shop</Link>
+                    <Link to="/account">My Account</Link>
                 </Col>
             </Row>
         </Container>
