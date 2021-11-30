321696a5a414ed3c4a64f75041c18e4573d0324e
admirkrilasevic <admirkrilasevic@gmail.com>
2021-11-30 22:57:07 +0100

    fixes

--- src/main/frontend/src/components/itemPage/BiddersSection.js
+++ src/main/frontend/src/components/itemPage/BiddersSection.js
@@ -1,15 +1,21 @@
 import styles from "./BiddersSection.module.css";
 import { Container, Col, Row } from 'react-bootstrap';
+import { useEffect, useState } from "react";
 
 function BiddersSection({bids}) {
+    
+    const [bidRows, setBidRows] = useState();
 
-    const bidRows = bids.map((bid) =>
-        <Row className={styles.contentRow} key={bid.id}>
-            <Col xs={8}>{bid.id}</Col>
-            <Col>{bid.date}</Col>
-            <Col className={styles.amountCol}>{bid.amount}</Col>
-        </Row>
-    );
+    useEffect(async () => {
+        if (bids){
+            setBidRows (bids.map((bid) =>
+            <Row className={styles.contentRow} key={bid.id}>
+                <Col xs={8}>{bid.id}</Col>
+                <Col>{(bid.date).substr(0,10)}</Col>
+                <Col className={styles.amountCol}>{bid.amount}</Col>
+            </Row>));
+        }
+    });
 
     return (
         <Row className={styles.biddersSection}>
