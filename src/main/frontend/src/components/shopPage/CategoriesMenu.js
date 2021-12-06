17030fdc07117e0ca59299a6e85cca77794d433a
admirkrilasevic <admirkrilasevic@gmail.com>
2021-12-06 15:33:37 +0100

    expanding subcategories

--- src/main/frontend/src/components/shopPage/CategoriesMenu.js
+++ src/main/frontend/src/components/shopPage/CategoriesMenu.js
@@ -5,10 +5,15 @@ import { fetchAllCategories } from "../landingPage/ItemService";
 function CategoriesMenu() {
 
     const [categoriesList, setCategoriesList] = useState([]);
+    const [expanded, setExpanded] = useState(false);
 
     useEffect(async () => {
         setCategoriesList(await fetchAllCategories());
     }, []);
+
+    const toggleExpandCollapse = () => {
+        setExpanded(!expanded);
+    };
 
     return (
         <div className={styles.menusContainer}>
@@ -23,20 +28,22 @@ function CategoriesMenu() {
                         <div>
                             <button className={styles.collapsibleCategory} key={category.id}>
                                 {category.name}
-                                <span className={styles.collapseIcon}>+</span>
+                                <span className={styles.collapseIcon} onClick={toggleExpandCollapse}>{expanded ? "-" : "+"}</span>
                             </button>
-                            <div className={styles.subcategoriesContainer}>
-                                {category.subcategories.map((subcategory) => (
-                                    <div className={styles.subcategoryItem}>
-                                        <input
-                                            className={styles.subcategoryCheckbox}
-                                            type="checkbox"
-                                            value={subcategory.id}
-                                        />
-                                        {subcategory.name + " (" + (subcategory.subcategoryItems.length ? subcategory.subcategoryItems.length : 0) + ")"}
-                                    </div>
-                                ))}
-                            </div>
+                            {expanded && (
+                                <div className={styles.subcategoriesContainer}>
+                                    {category.subcategories.map((subcategory) => (
+                                        <div className={styles.subcategoryItem}>
+                                            <input
+                                                className={styles.subcategoryCheckbox}
+                                                type="checkbox"
+                                                value={subcategory.id}
+                                            />
+                                            {subcategory.name + " (" + (subcategory.subcategoryItems.length ? subcategory.subcategoryItems.length : 0) + ")"}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
                     ))}
                 </div>
