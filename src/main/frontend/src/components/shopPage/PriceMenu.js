665cb92791330474118f40e35391340c8be7d245
admirkrilasevic <admirkrilasevic@gmail.com>
2021-12-14 17:37:41 +0100

    price slider

--- src/main/frontend/src/components/shopPage/PriceMenu.js
+++ src/main/frontend/src/components/shopPage/PriceMenu.js
@@ -7,37 +7,65 @@ function PriceMenu({items, setItems, priceRange, setPriceRange}) {
 
 	const [minPrice, setMinPrice] = useState(0);
     const [maxPrice, setMaxPrice] = useState(100);
+    const [localMinPrice, setLocalMinPrice] = useState(0);
+    const [localMaxPrice, setLocalMaxPrice] = useState(100);
     const [avgPrice, setAvgPrice] = useState(0);
 
 	useEffect(() => {
         loadPrices();
         setPriceRange([minPrice, maxPrice]);
+        setAvgPrice((maxPrice+minPrice)/2);
 	}, [items])
+
+    useEffect(() => {
+        setPriceRange([minPrice, maxPrice]);
+        setAvgPrice((maxPrice+minPrice)/2);
+        setLocalMinPrice(minPrice);
+        setLocalMaxPrice(maxPrice);
+    }, [maxPrice, minPrice])
 
     const loadPrices = () => {
         getMinPrice(items.map((item) => item.id)).then(response => setMinPrice(response));
         getMaxPrice(items.map((item) => item.id)).then(response => setMaxPrice(response));
-        getAvgPrice(items.map((item) => item.id)).then(response => setAvgPrice(response));
     }
 
-    const onSliderChange = (newValues) => {
+    const onSliderChange = (e, newValues) => {
         setPriceRange(newValues);
+        setLocalMinPrice(newValues[0]);
+        setLocalMaxPrice(newValues[1]);
+    }
+
+    const onMinInputChange = (e) => {
+  
+    }
+
+    const onMaxInputChange = (e) => {
+
     }
 
 	return (
 		<div className={styles.priceMenuContainer}>
 			<h6 className={styles.menuTitle}>FILTER BY PRICE</h6>
-            <div>
+            <div className={styles.priceInputs}>
                 <input 
-                    className={styles.slider}
-                    type="range"
-                    value={priceRange}
-                    min={minPrice}
-                    max={maxPrice}
-                    onChange={onSliderChange} 
+                    className={styles.minPriceInput}
+                    value={localMinPrice}
+                    onChange={onMinInputChange} 
+                />
+                <input 
+                    className={styles.maxPriceInput}
+                    value={localMaxPrice} 
+                    onChange={onMaxInputChange} 
                 />
             </div>
-            <p className={styles.priceRange}>${minPrice}-${maxPrice}</p>
+            <Slider
+                min={minPrice}
+                max={maxPrice}
+                value={priceRange}
+                step={0.1}
+                onChange={onSliderChange}
+            />
+            <p className={styles.priceRange}>${priceRange[0]}-${priceRange[1]}</p>
 			<p className={styles.priceAverage}>The average price is ${avgPrice.toFixed(2)}</p>
 		</div>
 	)
