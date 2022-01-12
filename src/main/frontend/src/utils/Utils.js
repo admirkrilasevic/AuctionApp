54a8e2b2f3f3ff06c299b12f23ab7c2e54b15046
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-12 17:54:44 +0100

    using floor instead of round

--- src/main/frontend/src/utils/Utils.js
+++ src/main/frontend/src/utils/Utils.js
@@ -12,8 +12,8 @@ export const calculateTimeLeft = (endDate) => {
     if (hours < 24) {
         return Math.round(hours) + " hours";
     } else if (hours >= 24 && hours < 168) {
-        return Math.round(hours/24) + " days " + Math.round(hours%24) + " hours" ;
+        return Math.floor(hours/24) + " days " + Math.round(hours%24) + " hours" ;
     } else {
-        return Math.round(hours/168) + " weeks " + Math.round((hours%168)/24) + " days";
+        return Math.floor(hours/168) + " weeks " + Math.round((hours%168)/24) + " days";
     }
 };
