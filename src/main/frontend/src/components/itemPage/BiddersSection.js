a3e6ad830cdbffdbdfb86ef7e278e0e7b6547c8e
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-20 01:08:33 +0100

    added pagination to bidders table

--- src/main/frontend/src/components/itemPage/BiddersSection.js
+++ src/main/frontend/src/components/itemPage/BiddersSection.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 function BiddersSection({bids}) {
     
     const [bidDetails, setBidDetails] = useState([]);
+    const [limit, setLimit] = useState(5);
 
     useEffect(async () => {
         if (bids)
@@ -31,13 +32,14 @@ function BiddersSection({bids}) {
                     <Col>Date</Col>
                     <Col>Bid</Col>
                 </Row>
-                {bidDetails.map((bid) =>
+                {bidDetails.sort((a,b) => b.amount - a.amount).slice(0,limit).map((bid) =>
                 <Row className={styles.contentRow} key={bid.id}>
                     <Col xs={8}>{bid.user.name} {bid.user.surname}</Col>
                     <Col>{formatDate(bid.date)}</Col>
                     <Col className={styles.amountCol}>$ {bid.amount}</Col>
                 </Row>)}
             </Container>
+            {(bidDetails.length > limit) && <div><button onClick={() => setLimit(limit+5)} className={styles.loadMoreButton}>LOAD MORE BIDS</button></div>}
             </Row>)
         }
         </div>
