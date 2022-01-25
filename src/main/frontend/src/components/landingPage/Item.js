4d1b3d80d1d46d51dae0c51ab9ab03b9c0ec947c
admirkrilasevic <admirkrilasevic@gmail.com>
2022-01-25 11:43:20 +0100

    image split fix

--- src/main/frontend/src/components/landingPage/Item.js
+++ src/main/frontend/src/components/landingPage/Item.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Item(props) {
     return (
       <div className = {styles.itemContainer}>
-        <Link to={`/items/${props.id}`}><img className = {styles.photo} src={props.photo} alt="" /></Link>
+        <Link to={`/items/${props.id}`}><img className = {styles.photo} src={props.photo.split(";")[0]} alt="" /></Link>
         <Link to={`/items/${props.id}`} className={styles.itemLink}><h3 className={styles.title}>{props.name}</h3></Link> 
         <p className= {styles.startPrice} >
           Start From <span className= {styles.amount}>${props.startingPrice}</span>
